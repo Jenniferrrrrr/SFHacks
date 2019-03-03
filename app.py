6afe80af2d249f4dc9196a2ffec2ed17cbed0e52
Chirasree Mandal <chirasree.mandal@gmail.com>
2019-03-02 21:36:46 -0800

    fix routing with jinja in html, add gitignore

--- app.py
+++ app.py
@@ -5,22 +5,40 @@ import random
 import ipinfo
 import Scraper
 import json
+from flask_bootstrap import Bootstrap
 
 with open('config.json') as f:
     api_keys = json.load(f)
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+local_resources = {}
 
 @app.route('/')
 def home():
+    global local_resources
     zipcode = getZipCode()
     local_resources = Scraper.scraper(zipcode)
-    render_template('index.html', local_resources = local_resources)
+    names = local_resources.keys() 
+
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    render_template('about')
+    return render_template('about.html')
  
+@app.route('/fw19')
+def fw19():
+    return render_template("fw19.html")
+
+@app.route('/fw18')
+def fw18():
+    return render_template("fw18.html")
+
+@app.route('/ss19')
+def ss19():
+    return render_template("ss19.html")
 
 def getZipCode():
     ip_access_token = api_keys["ip_key"]
