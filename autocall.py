3302aa2e42bcab89eae8ca1ddda62e8ee0990a7f
Serenade092 <serenade092@berkeley.edu>
2019-03-02 22:57:34 -0800

    changed ip address

--- autocall.py
+++ autocall.py
@@ -7,21 +7,22 @@ from urllib.parse import urlencode
 with open('twil.json') as f:
     api_keys = json.load(f)
 
-dictionary = Scraper.scraper("94720") #Dummy call
+
 def resources(dic):
     m = ""
     for item in dic:
         m = m + item + " at " + dic[item] + " , "
     return m
 
-def call():
+def call(ipadress):
     """
     Some example usage of different twilio resources.
     """
+    dictionary = Scraper.scraper(ipadress) 
     client = Client(api_keys["id"], api_keys["key"])
     message = "Here are local resources that you can find help from: " + resources(dictionary)
     client.calls.create(to='+15109449663',from_='+16107569992', url="https://twimlets.com/message?" + urlencode({'Message': message}))
 
 if __name__ == '__main__':
-    call()
+    call(ipadress)
 
